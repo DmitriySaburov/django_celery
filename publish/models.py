f003a0da118bf5f788349dc28c812e5b8abdc92e
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-08-06 14:05:49 +0300

    добавил задачу для отправки кол-ва просмотров

--- publish/models.py
+++ publish/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
@@ -13,6 +14,10 @@ class Post(models.Model):
     title = models.CharField(verbose_name="Title", max_length=200)
     content = models.TextField(verbose_name="Content")
     slug = models.SlugField(verbose_name="Slug")
+    view_count = models.IntegerField(verbose_name="View Count", default=0)
+    
+    def get_absolute_url(self):
+        return reverse("view_post", args=[self.slug])
     
     def __str__(self):
         return f"{self.title} by {self.author}"

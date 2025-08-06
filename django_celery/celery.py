e395757a9518450978c175554bfe43fc62a25756
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-08-06 14:58:09 +0300

    создал переодическую отправку сообщения

--- django_celery/celery.py
+++ django_celery/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 
 
@@ -13,3 +14,9 @@ app = Celery(
 app.config_from_object("django.conf:settings")
 
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    "send-report-every-single-minute": {
+        "task": "publish.tasks.send_view_count_report",
+        "schedule": crontab(minute=0, hour=0),
+    }
+}
